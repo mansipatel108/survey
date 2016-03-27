2cb098d2b7adb2200fcf44fd6350d85fced6b1e0
Mansi <mpatel.y@gmail.com>
2016-03-27 14:18:40 -0400

    updated db.ts

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'url': 'mongodb://lab5:lab5@ds064628.mlab.com:64628/lab5'
+    'url': 'mongodb://survey:survey@ds028559.mlab.com:28559/survey'
 };
 
 //# sourceMappingURL=db.js.map
